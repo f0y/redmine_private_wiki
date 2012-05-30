26c0618fddec29a32864f70f96993a3217ad41cf
Oleg Kandaurov <kandaurovoleg@gmail.com>
2012-05-30 21:25:41 +0400

    Update Readme

--- init.rb
+++ init.rb
@@ -22,8 +22,8 @@ Redmine::Plugin.register :redmine_private_wiki do
   name 'Private Wiki'
   author 'Oleg Kandaurov'
   description 'Adds private pages to wiki'
-  version '0.1'
-  author_url 'http://okandaurov.info'
+  version '0.2.1'
+  author_url 'http://f0y.me'
   requires_redmine :version_or_higher => '2.0.0'
 
   project_module :wiki do
